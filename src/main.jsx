c9da2ca729cf7e90144b3cf545b4f956a13da134
Fidel Lascano <lascano.valencia.fidel@gmail.com>
2022-12-21 17:32:39 -0500

    Initial project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
+import JournalApp from "./JournalApp.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
