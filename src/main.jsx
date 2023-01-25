e81e51250573b0c6effa8d362c93f7008b39adbf
lascano.valencia.fidel@gmail.com <Df3a0b951>
2023-01-24 23:30:02 -0500

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom";
 import './style.css'
 import JournalApp from "./JournalApp.jsx";
+import {Provider} from "react-redux";
+import {store} from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <Provider store={store}>
     <BrowserRouter>
       <JournalApp />
     </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
 )
